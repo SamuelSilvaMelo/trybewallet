e2a0229d3c63097c30c45e895fc56a5ce60d2ea5
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-09-12 12:38:53 -0300

    Updates

---
+++ precache-manifest.736db454135f88f211b8e059841a3af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89bfc0e763bf7892a35fd535d6575cc",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "461acae141f8923569ad",
+    "url": "/trybewallet/static/css/main.4f022533.chunk.css"
+  },
+  {
+    "revision": "95ab53740d86b2f5694a",
+    "url": "/trybewallet/static/js/2.2ece33aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.2ece33aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "461acae141f8923569ad",
+    "url": "/trybewallet/static/js/main.37c67d48.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  }
+]);
